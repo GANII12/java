f0d5ee463c5341a217fe65e016bb9cc907b5d87b
GANII12 <ganeshrock618@gmail.com>
2022-02-03 22:35:58 +0530

    assignment

---
+++ src/com/company/comparision.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class comparision {
+    public static void main(String[] args) {
+            scanner sc = new scanner(system.in);
+            Integer integer1 = sc.next.Int();
+            Integer integer2 = 20;
+        System.out.println("compare output: "+integer1.compareTo(integer2));
+
+    }
+
+}
+
