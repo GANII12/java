a9dda2d26d225c5e335a2b3e9c9b4b2189c2e5b2
GANII12 <ganeshrock618@gmail.com>
2022-02-04 21:49:33 +0530

    assignment1 :day3

--- src/com/company/comparision.java
+++ src/com/company/comparision.java
@@ -2,9 +2,10 @@ package com.company;
 import java.util.*;
 public class comparision {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            Integer integer1 = 10;
-            Integer integer2 = 20;
+        System.out.println("enter the number:");
+        Scanner sc = new Scanner(System.in);
+        Integer integer1 = sc.nextInt();
+        Integer integer2 = 20;
         System.out.println("compare output: "+integer1.compareTo(integer2));
     }
 }
