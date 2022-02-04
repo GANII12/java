270df3b083ef366e8ef10246d7d50bf3f6d5cff6
GANII12 <ganeshrock618@gmail.com>
2022-02-04 15:54:52 +0530

    assignment

--- src/com/company/comparision.java
+++ src/com/company/comparision.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class comparision {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            Integer integer1 = sc.nextInt();
+            Integer integer1 = 10;
             Integer integer2 = 20;
         System.out.println("compare output: "+integer1.compareTo(integer2));
     }
